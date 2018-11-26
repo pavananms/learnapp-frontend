189c554270a13592fa39be461b1cf05e26ceb993
deepakdas777 <deepakdasvkd007@gmail.com>
2018-11-26 22:09:07 +0530

    coresual added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Background from './background.jpg';
+import img1 from './1.jpg';
+import img2 from './2.jpg';
+import img3 from './3.jpg';
 import Title from './Title';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 //class App extends Component {
 //  render() {
@@ -31,10 +36,9 @@ import Title from './Title';
 //  }
 //}
 //
-
 var sectionStyle = {
         width: "100%",
-        height: "720px",
+        height: "100%",
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
         backgroundSize: "cover",
@@ -50,6 +54,27 @@ class Section extends Component {
   }
 }
 
+class DemoCarousel extends Component {
+  render() {
+      return (
+          <Carousel>
+              <div>
+                  <img src={img1} />
+                  {/* <p className="legend">Legend 1</p> */}
+              </div>
+              <div>
+                  <img src={img2} />
+                  {/* <p className="legend">Legend 2</p> */}
+              </div>
+              <div>
+                  <img src={img3} />
+                  {/* <p className="legend">Legend 3</p> */}
+              </div>
+          </Carousel>
+      );
+  }
+}
+
 class App extends Component {
   render(){
     return(
@@ -62,7 +87,10 @@ class App extends Component {
                 </Typography>
               </Toolbar>
             </AppBar>
-            <Title title={'We offer'}/>
+            <DemoCarousel>
+            </DemoCarousel>
+            {/* <Title title={'We offer'}/> */}
+            {/* <Title title={'Here I am'}/> */}
             {/* <Section> */}
             {/* </Section>           */}
           </div>
